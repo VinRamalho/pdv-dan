7eebd0c49b4fcfab4cddfb5f2675851f3340ad42
VinRamalho <vinramalho10@gmail.com>
2024-04-20 02:39:25 -0300

    feat: add bull-board

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, RequestMethod } from '@nestjs/common';
 import { QueueMailService } from 'src/queue/mail/mail.producer.service';
 import { QueueMailConsumer } from './mail/mail.consumer';
 import { UserService } from 'src/users/user.service';
-import { BullModule } from '@nestjs/bull';
+import { BullModule, InjectQueue } from '@nestjs/bull';
 import { QueueDto } from './dto/queue.dto';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UserSchema, UserSchemaFactory } from 'src/users/schemas/user.schema';
+import { Queue } from 'bull';
+import { MiddlewareBuilder } from '@nestjs/core';
+import { createBullBoard } from 'bull-board';
+import { BullAdapter } from 'bull-board/bullAdapter';
 
 @Module({
   imports: [
@@ -20,4 +24,11 @@ import { UserSchema, UserSchemaFactory } from 'src/users/schemas/user.schema';
   providers: [QueueMailService, QueueMailConsumer, UserService],
   exports: [QueueMailService, QueueMailConsumer],
 })
-export class QueueModule {}
+export class QueueModule {
+  constructor(@InjectQueue(QueueDto.SEND_EMAIL) private queue: Queue) {}
+
+  configure(consumer: MiddlewareBuilder) {
+    const { router } = createBullBoard([new BullAdapter(this.queue)]);
+    consumer.apply(router).forRoutes('admin/queues');
+  }
+}
