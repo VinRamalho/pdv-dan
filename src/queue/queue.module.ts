7148e5d505a0db52761de7e10a93f258ac8c49f6
VinRamalho <vinramalho10@gmail.com>
2024-04-20 01:02:34 -0300

    fix: add redis route

--- src/queue/queue.module.ts
+++ src/queue/queue.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { QueueMailService } from 'src/queue/mail/mail.producer.service';
 import { QueueMailConsumer } from './mail/mail.consumer';
+import { UserService } from 'src/users/user.service';
 
 @Module({
   imports: [],
   controllers: [],
-  providers: [QueueMailService, QueueMailConsumer],
+  providers: [QueueMailService, QueueMailConsumer, UserService],
   exports: [QueueMailService, QueueMailConsumer],
 })
 export class QueueModule {}
