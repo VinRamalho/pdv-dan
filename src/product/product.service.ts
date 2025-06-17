077fc22f77b19122d44e516709c4142e45464701
VinRamalho <vinramalho10@gmail.com>
2025-06-17 00:13:51 -0300

    feat: add conflict handling in create method of ProductService to prevent duplicate product titles

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ConflictException } from '@nestjs/common';
 import { Model, HydratedDocument } from 'mongoose';
 import { ProductDocument, ProductSchema } from './schemas/product.schema';
 import { Crud } from 'src/crud/crud.abstract';
 import { InjectModel } from '@nestjs/mongoose';
 import { DataStatus } from 'src/data/dto/data.dto';
+import { MongoError } from 'mongodb';
 
 @Injectable()
 export class ProductService extends Crud<ProductSchema> {
@@ -12,6 +13,23 @@ export class ProductService extends Crud<ProductSchema> {
     private readonly productModel: Model<ProductSchema>,
   ) {
     super(productModel);
+  }
+
+  async create(
+    entity: Partial<ProductSchema>,
+  ): Promise<HydratedDocument<ProductSchema>> {
+    try {
+      return await super.createData(entity);
+    } catch (error) {
+      console.log(error instanceof MongoError);
+      console.log(error.code);
+      if (error instanceof MongoError && error.code === 11000) {
+        throw new ConflictException(
+          `A product with title '${entity.title}' already exists`,
+        );
+      }
+      throw error;
+    }
   }
 
   async findByUserId(
